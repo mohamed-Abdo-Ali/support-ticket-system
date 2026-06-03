332b206c2d5c111c6d8a42ee774167a0cfe0d2c9
mhamed <www.mhmd2778@gmail.com>
2026-06-03 08:56:03 +0300

    تغيير مسارات القوالب لبرطها مع dist في الفيو

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-6^jz#2k*36_a%xepv-4iq3ncvo)#ck54ss(k+&@vp6^7j5d27_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mohammed1ali.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], 
+        'DIRS': [BASE_DIR / 'frontend/dist'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'support_db',
-        'USER': 'root',    
-        'PASSWORD': '',  
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'support_db',
+#         'USER': 'root',    
+#         'PASSWORD': '',  
+#         'HOST': 'localhost',
+#         'PORT': '3306',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
