9ba00aad8bb58bdd825abde7c292351c416d2add
mhamed <www.mhmd2778@gmail.com>
2026-06-03 10:34:32 +0300

    ok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/static/', // 👈 أضف هذا السطر هنا بدقة ليصبح مسار الملفات صحيحاً لجينغو
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
